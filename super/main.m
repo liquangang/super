fc3fd824eba8762771163f1f1abf238f9b2e6e4c
liquangang <liquangang@idstaff.com>
2017-04-10 12:08:10 +0800

    提交项目代码

---
+++ super/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  super
+//
+//  Created by quangang on 2017/4/10.
+//  Copyright © 2017年 必要. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
